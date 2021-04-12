e76bf9d1b925256ac9ab4f37c29d75935b80fea6
nikolaswise <nikolaswise@gmail.com>
2021-04-12 12:34:48 -0700

    stash

--- src/components/Actions.svelte
+++ src/components/Actions.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { resign, pass } from './game-actions.js'
+  // import { resign, pass } from './game-actions.js'
 
   export let game
   export let games
   export let color
 
-  submitMove(game, games, board.getCurrentPlayer())
+  // submitMove(game, games, board.getCurrentPlayer())
 </script>
 
 <div class="actions">
